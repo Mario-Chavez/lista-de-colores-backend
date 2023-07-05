c9b09cb0a20b17200b1c3eb66131eb06b3a59ec7
Mario-Chavez <marioachavez1@gmail.com>
2023-07-05 16:30:15 -0300

    se agrego el controlador de borrarColorr

--- src/controllers/color.controller.js
+++ src/controllers/color.controller.js
@@ -30,7 +30,7 @@ export const borrarColor = async (req, res) => {
     try {
         const { id } = req.params;
         await Producto.findByIdAndDelete(id);
-        res.status(200).json({ mensaje: "color eliminiado" });
+        res.status(200).json({ mensaje: "color eliminado" });
     } catch (error) {
         res.status(404).json({
             mensaje: "error al borrar producto no se encontro en db",
